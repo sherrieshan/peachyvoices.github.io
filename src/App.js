659d67327f7ba5e9b0c3200fc2d3af773d8410a4
Sherrie Shan <sherrie.the.shan@gmail.com>
2018-01-29 19:38:36 -0800

    Started landing page.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import landing from './images/landing.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <ul className="app-menu">
+          <li>Demos</li>
+          <li style={{fontSize: 60+'px', color:'white'}}>Sherrie Voices</li>
+          <li>About Me</li>
+        </ul>
+        <div className="app-landing">
+          <img src={landing} alt="landing"/>
+          <h1>Hi, I'm Sherrie!</h1>
+          <p>I'm a voice actress.</p>
+        </div>
       </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
     );
   }
 }
