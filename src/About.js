23d4fca93a4bce20e25ab273b53727de2a04edc9
Sherrie Shan <sherrie.the.shan@gmail.com>
2019-07-31 12:14:20 -0700

    adjusted contact form, added kofi button

--- src/About.js
+++ src/About.js
@@ -3,6 +3,7 @@ import './About.css';
 import headshot from './images/headshot_square.jpg';
 
 class About extends Component {
+
     render() {
         return(
             <div id="about">
@@ -22,6 +23,8 @@ class About extends Component {
                         If you have any questions or would like a copy of my resume, please contact me with the form above!
                         <br/><br/>
                         You can also find me on <a href="https://www.fiverr.com/sherrieshan/voice-your-female-characters">fiverr</a>!
+                        <br/><br/><br/><br/>
+                        <a href='https://ko-fi.com/X8X310D1M' target='_blank'><img height='36' style={{border:'0', height:36+'px'}} src='https://az743702.vo.msecnd.net/cdn/kofi4.png?v=2' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
                     </p>
                 </div>
             </div>
